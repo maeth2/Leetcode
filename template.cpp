a6c8a87391e754948325678947924d6925ca99d6
1maeth2 <1maeth2@hdsb.ca>
2023-09-23 09:25:21 -0400

    Added Solution Template.

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+using ll = long long;
+
+void solve(){
+}
+
+int main(void){
+    solve();
+    return 1;
+}
