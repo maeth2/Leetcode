508500d8728b0e5132dfd916bed3237f8d15b0fe
1maeth2 <1maeth2@hdsb.ca>
2023-09-30 11:58:23 -0400

    Updated Solution for Reverse Integer.

--- medium/reverse_integer.cpp
+++ medium/reverse_integer.cpp
@@ -8,7 +8,21 @@
 using namespace std;
 using ll = long long;
 
+
 int reverse(int x) {
+    int r=0; 
+    while(x){
+        if (r>INT_MAX/10 || r<INT_MIN/10) return 0;
+        r=r*10+x%10;
+        x=x/10;
+    } 
+    return r;
+}
+
+/*
+    This works if input is greater than 32 bit
+*/
+int reverse1(int x) {
     string max = "2147483647";
     string num = "";
     int co = 1;
