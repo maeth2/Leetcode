8a222669ee409a0220cbb0d11f7c59656975358d
1maeth2 <1maeth2@hdsb.ca>
2023-10-03 23:48:16 -0400

    Added Solution for Design Hashmap.

---
+++ easy/design_hashmap.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+using ll = long long;
+
+class MyHashMap {
+public:
+    vector<int> hashMap;
+    MyHashMap() {
+        vector<int> temp(1000001, -1);
+        hashMap = temp;
+    }
+    
+    void put(int key, int value) {
+        hashMap[key] = value;
+    }
+    
+    int get(int key) {
+        return hashMap[key];
+    }
+    
+    void remove(int key) {
+        hashMap[key] = -1;
+    }
+};
+
+void solve(){
+}
+
+int main(void){
+    solve();
+    return 1;
+}
