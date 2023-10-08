1882b025767196e909ff7df237c29aa74bdd0636
1maeth2 <1maeth2@hdsb.ca>
2023-10-07 23:01:23 -0400

    Updated Solution for Largest Rectangle in Histogram.

--- hard/largest_rectangle_in_histogram.cpp
+++ hard/largest_rectangle_in_histogram.cpp
@@ -12,14 +12,28 @@ using ll = long long;
 /*
     NOTE: To solve this problem we can use a stack to keep track of the heights.
 
-    For every bar ‘x’, we calculate the area with ‘x’ as the smallest bar in the rectangle. 
-    If we calculate the such area for every bar ‘x’ and find the maximum of all areas, our task is done. 
-
-    To do this, we have to keep a stack of bar heights so we can find the first instance where 'x' is not the smallest bar.
-
     We will iterate through the array. If the current bar is greater than the last one, we will add it to the stack. If not, then we will
-    loop through the stack and checking the area of the rectangle using the height and the distance from the top index as the width
+    loop through the stack and checking the area of the rectangle using the top stack as the height and the distance from the top index to the current as the width
     (We can do this since the stack will always be in descending order) until the height is less than the current bar.
+
+    Example; This is a stack with size 5. The current number is 2. Lets represent the top of the stack as i and the current index as j.
+
+            O                   |
+          | O                 O O
+        | | O               | O O
+      | | | O X           | | O O X
+    | | | | O X         | | | O O X     
+            i j               i   j     
+
+            |                   |
+          | |                 | |
+        O O O               | | |
+      | O O O X           O O O O X
+    | | O O O X         | O O O O X     
+        i     j           i       j
+
+    As you can see, the stack is in decreasing order, meaning the top of the stack is always larger or equal than the one behind it. For a given range of bars, the height is determined
+    by the height of the shortest bar. Therefore for each element in the stack, there will always be a rectangle of height[i] and width (j - i).
 */
 
 int largestRectangleArea(vector<int>& heights) {
