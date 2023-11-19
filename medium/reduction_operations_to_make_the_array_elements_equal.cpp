a549518eac66048949de9354fd9038991e86bc5a
1maeth2 <1maeth2@hdsb.ca>
2023-11-19 08:18:39 -0500

    Added Solution for Reduction Operations to Make the Array Elements Equal.

---
+++ medium/reduction_operations_to_make_the_array_elements_equal.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+using ll = long long;
+
+int reductionOperations(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    vector<int> count;
+    int n = nums.size();
+    int prev = nums[0];
+    int c = 0;
+    for(int i : nums) cout << i << " ";
+    cout << "\n";
+    for(int i = 0; i < nums.size(); i++){
+        int v = nums[i];
+        if(prev != v){
+            count.push_back(c);
+            c = 0;
+        }
+        prev = v;
+        c++;
+    }
+    count.push_back(c);
+    for(int i : count) cout << i << ' ';
+    cout << "\n";
+    int ans = 0;
+    int prevTotal = 0;
+    for(int i = count.size() - 1; i >= 1; i--){
+        prevTotal += count[i];
+        ans += prevTotal;
+    }
+    return ans;
+}
+
+void solve(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) cin >> nums[i];
+    cout << reductionOperations(nums);
+}
+
+int main(void){
+    solve();
+    return 1;
+}
